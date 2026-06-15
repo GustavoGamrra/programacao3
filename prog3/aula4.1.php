547c0bfb6ed6c660fcc4d086c6bc1c27dfc34358
GGgamigus <gustavo.14050@aluno.iffar.edu.br>
2026-06-15 11:33:00 -0300

    Update aula4.1.php

--- prog3/aula4.1.php
+++ prog3/aula4.1.php
@@ -1,1 +1,49 @@
+<?php
 
+class Funcionario {
+    public string $nome;
+    public float $salario;
+
+    public function __construct(string $nome, float $salario){
+        $this->nome = $nome;
+        $this-> salario = $salario;
+    }
+
+  public function calcularBonus(): float {
+            return $this->salario * 0.10;
+    }
+
+}
+
+class Gerente extends Funcionario {
+
+  public function calcularBonus(): float {
+            return $this->salario * 0.20;
+    }
+}
+
+class Desenvolvedor extends Funcionario{
+ public function calcularBonus(): float {
+            return $this->salario * 0.15;
+    }
+}
+
+$gerente = new Gerente ("joão", 20000);
+
+$desenvolvedor = new Desenvolvedor("maria", 8000);
+
+echo "nome: " . $gerente->nome . "\n";
+echo "cargo: gerente\n";
+echo "bônus" . $gerente->calcularBonus() . "\n\n";
+
+echo "nome: " . $desenvolvedor->nome . "\n";
+echo "cargo: desenvolvedor";
+echo "bônus: ". $desenvolvedor->calcularBonus();
+
+
+
+
+
+
+
+?>
